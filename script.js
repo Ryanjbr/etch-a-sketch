a63969d4af7be0c29f14c759e1bc1a4a46a1d0c7
Ryan Bridge <ryanbridgemusic@gmail.com>
2022-07-21 10:22:21 -0400

    Add slider to allow user to choose size of grid
    Changed layout to CSS grid, because flexbox layout broke at a certain
    size.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const container = document.querySelector('#container')
+const slider = document.querySelector("#range")
+let sliderValue = 50;
 
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.setAttribute("class", "gridbox");
-    container.appendChild(div)
+slider.oninput = function () {
+    console.log(slider.value)
+    container.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`
+    container.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+    for (let i = 0; i < slider.value*slider.value; i++) {
+        const div = document.createElement('div');
+        div.setAttribute("class", "gridbox");
+        container.appendChild(div)
+    }
 }
+
