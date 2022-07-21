2d94bd5a3eaddbf76a7505ef3f9d69acf3c07c0d
Ryan Bridge <ryanbridgemusic@gmail.com>
2022-07-21 14:17:27 -0400

    Added hover reset

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('#container');
 const slider = document.querySelector("#range");
 const size = document.querySelector("#gridSize");
+const reset = document.querySelector("#reset")
 let sliderValue = 10;
 
 createGrid(10); 
@@ -9,6 +10,10 @@ slider.oninput = function() {
     size.textContent = slider.value;
     createGrid(slider.value);
 };
+
+reset.addEventListener('mouseover', () => {
+    createGrid(slider.value)
+})
 
 function createGrid(value) {
     while(container.firstChild) {
@@ -24,19 +29,14 @@ function createGrid(value) {
     let boxes = document.querySelectorAll('.gridbox')
     for(let box of boxes) {
         box.addEventListener('mouseover', () => {
-                box.dataset.red = Math.floor(Math.random() * 255)
-                box.dataset.green = Math.floor(Math.random() * 255)
-                box.dataset.blue = Math.floor(Math.random() * 255)
-                box.style.backgroundColor = `rgb(${box.dataset.red}, ${box.dataset.green}, ${box.dataset.blue})`
-                box.setAttribute('data-initial', 'true')
+                box.dataset.color = Math.floor(Math.random() * 255)
+                box.style.backgroundColor = `rgb(${box.dataset.color}, ${box.dataset.color}, ${box.dataset.color})`
         }, {once : true});
     }
     for(let box of boxes) {
         box.addEventListener('mouseover', () => {
-            box.dataset.red = box.dataset.red - 35;
-            box.dataset.green = box.dataset.green - 35;
-            box.dataset.blue = box.dataset.blue - 35;
-            box.style.backgroundColor = `rgb(${box.dataset.red}, ${box.dataset.green}, ${box.dataset.blue})`
+            box.dataset.color = box.dataset.color - 35;
+            box.style.backgroundColor = `rgb(${box.dataset.color}, ${box.dataset.color}, ${box.dataset.color})`
         });
     }
 }
